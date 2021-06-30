4fe838371ab69650bad51339d57aa10ab2467f23
cgiossi <62955751+CristinaGiossi@users.noreply.github.com>
2021-06-30 19:08:56 +0200

    Add files via upload
    New framework pipeline+
    Extra functions: helpers (default values) and modifiers

---
+++ cbgt2-main/init_params.py
@@ -0,0 +1,256 @@
+from frontendhelpers import *
+import copy
+
+
+def helper_celldefaults():
+
+    celldefaults = {'N': 75,
+                    'C': 0.5,
+                    'Taum': 20,
+                    'RestPot': -70,
+                    'ResetPot': -55,
+                    'Threshold': -50,
+                    'RestPot_ca': -85,
+                    'Alpha_ca': 0.5,
+                    'Tau_ca': 80,
+                    'Eff_ca': 0.0,
+                    'tauhm': 20,
+                    'tauhp': 100,
+                    'V_h': -60,
+                    'V_T': 120,
+                    'g_T': 0,
+                    'g_adr_max': 0,
+                    'Vadr_h': -100,
+                    'Vadr_s': 10,
+                    'ADRRevPot': -90,
+                    'g_k_max': 0,
+                    'Vk_h': -34,
+                    'Vk_s': 6.5,
+                    'tau_k_max': 8,
+                    'n_k': 0,
+                    'h': 1, }
+
+    return celldefaults
+
+
+def modifier_celldefaults(celldefaults, params=dict()):
+
+    celldefaults_modified = {}
+    celldefaults_modified = copy.deepcopy(celldefaults)
+
+    if len(params) != 0:
+
+        celldefaults_modified.update(params)
+
+    return celldefaults_modified
+
+
+def helper_popspecific():
+
+    popspecific = {'LIP': {'N': 204},
+                   'FSI': {'C': 0.2, 'Taum': 10},
+                   # should be 10 but was 20 due to bug
+                   'GPeP': {'N': 750, 'g_T': 0.06, 'Taum': 20},
+                   'STNE': {'N': 750, 'g_T': 0.06},
+                   'LIPI': {'N': 186, 'C': 0.2, 'Taum': 10},
+                   'Th': {'Taum': 27.78}}
+
+    return popspecific
+
+
+def modifier_popspecific(popspecific, pops=dict()):
+
+    popspecific_modified = {}
+    popspecific_modified = copy.deepcopy(popspecific)
+
+    if len(pops) != 0:
+        for key in pops.keys():
+            for item in pops[key].keys():
+                popspecific_modified[key][item] = pops[key][item]
+
+    return popspecific_modified
+
+
+def helper_receptordefaults():
+
+    receptordefaults = {'Tau_AMPA': 2,
+                        'RevPot_AMPA': 0,
+                        'Tau_GABA': 5,
+                        'RevPot_GABA': -70,
+                        'Tau_NMDA': 100,
+                        'RevPot_NMDA': 0, }
+
+    return receptordefaults
+
+
+def modifier_receptordefaults(receptordefaults, receps=dict()):
+
+    receptordefaults_modified = {}
+    receptordefaults_modified = copy.deepcopy(receptordefaults)
+
+    if len(receps) != 0:
+
+        receptordefaults_modified.update(receps)
+
+    return receptordefaults_modified
+
+
+def helper_basestim():
+
+    basestim = {'FSI': {
+        'FreqExt_AMPA': 3.6,
+        'MeanExtEff_AMPA': 1.55,
+        'MeanExtCon_AMPA': 800},
+        'LIPI': {
+        'FreqExt_AMPA': 1.05,
+        'MeanExtEff_AMPA': 1.2,
+        'MeanExtCon_AMPA': 640},
+        'GPi': {
+        'FreqExt_AMPA': 0.8,
+        'MeanExtEff_AMPA': 5.9,
+        'MeanExtCon_AMPA': 800},
+        'STNE': {
+        'FreqExt_AMPA': 4.45,
+        'MeanExtEff_AMPA': 1.65,
+        'MeanExtCon_AMPA': 800},
+        'GPeP': {
+        'FreqExt_AMPA': 4,
+        'MeanExtEff_AMPA': 2,
+        'MeanExtCon_AMPA': 800,
+        'FreqExt_GABA': 2,
+        'MeanExtEff_GABA': 2,
+        'MeanExtCon_GABA': 2000},
+        'D1STR': {
+        'FreqExt_AMPA': 1.3,
+        'MeanExtEff_AMPA': 4,
+        'MeanExtCon_AMPA': 800},
+        'D2STR': {
+        'FreqExt_AMPA': 1.3,
+        'MeanExtEff_AMPA': 4,
+        'MeanExtCon_AMPA': 800},
+        'LIP': {
+        'FreqExt_AMPA': 2.2,
+        'MeanExtEff_AMPA': 2,
+        'MeanExtCon_AMPA': 800},
+        'Th': {
+        'FreqExt_AMPA': 2.2,
+        'MeanExtEff_AMPA': 2.5,
+        'MeanExtCon_AMPA': 800}, }
+
+    return basestim
+
+
+def modifier_basestim(basestim, base=dict()):
+
+    basestim_modified = {}
+    basestim_modified = copy.deepcopy(basestim)
+
+    if len(base) != 0:
+        for key in base.keys():
+            for item in base[key].keys():
+                basestim_modified[key][item] = base[key][item]
+
+    return basestim_modified
+
+
+def helper_dpmndefaults():
+
+    dpmndefaults = {'dpmn_tauDOP': 2,
+                    'dpmn_alpha': 0.3,
+                    'dpmn_DAt': 0.0,
+                    'dpmn_taum': 1e100,
+                    'dpmn_dPRE': 0.8,
+                    'dpmn_dPOST': 0.04,
+                    'dpmn_tauE': 15,
+                    'dpmn_tauPRE': 15,
+                    'dpmn_tauPOST': 6,
+                    'dpmn_wmax': 0.3,
+                    'dpmn_w': 0.1286,
+                    'dpmn_Q1': 0.0,
+                    'dpmn_Q2': 0.0,
+                    'dpmn_m': 1.0,
+                    'dpmn_E': 0.0,
+                    'dpmn_DAp': 0.0,
+                    'dpmn_APRE': 0.0,
+                    'dpmn_APOST': 0.0,
+                    'dpmn_XPRE': 0.0,
+                    'dpmn_XPOST': 0.0}
+
+    return dpmndefaults
+
+
+def modifier_dpmndefaults(dpmndefaults, dpmns=dict()):
+
+    dpmndefaults_modified = {}
+    dpmndefaults_modified = copy.deepcopy(dpmndefaults)
+
+    if len(dpmns) != 0:
+
+        dpmndefaults_modified.update(dpmns)
+
+    return dpmndefaults_modified
+
+
+def helper_d1defaults(d1=dict()):
+
+    d1defaults = {'dpmn_type': 1,
+                  'dpmn_alphaw': 55 / 3.0,  # ???
+                  'dpmn_a': 1.0,
+                  'dpmn_b': 0.1,
+                  'dpmn_c': 0.05, }
+
+    return d1defaults
+
+
+def modifier_d1defaults(d1defaults, d1=dict()):
+
+    d1defaults_modified = {}
+    d1defaults_modified = copy.deepcopy(d1defaults)
+
+    if len(d1) != 0:
+
+        d1defaults_modified.update(d1)
+
+    return d1defaults_modified
+
+
+def helper_d2defaults(d2=dict()):
+
+    d2defaults = {'dpmn_type': 2,
+                  'dpmn_alphaw': -45 / 3.0,
+                  'dpmn_a': 0.5,
+                  'dpmn_b': 0.005,
+                  'dpmn_c': 0.05, }
+
+    return d2defaults
+
+
+def modifier_d2defaults(d2defaults, d2=dict()):
+
+    d2defaults_modified = {}
+    d2defaults_modified = copy.deepcopy(d2defaults)
+
+    if len(d2) != 0:
+
+        d2defaults_modified.update(d2)
+
+    return d2defaults_modified
+
+
+def helper_actionchannels(channels=dict()):
+
+    actionchannels = {'action': [1, 2], }
+
+    return actionchannels
+
+
+def modifier_actionchannels(actionchannels, channels=dict()):
+
+    actionchannels_modified = {}
+    actionchannels_modified = copy.deepcopy(actionchannels)
+
+    if len(channels) != 0:
+
+        actionchannels_modified.update(channels)
+
+    return actionchannels_modified
---
+++ cbgt2-main/phaseAsegment2.py
@@ -0,0 +1,155 @@
+# generating reward schedule for conflict/volatility tasks
+# important output variables
+# - t1_epochs
+# - t2_epochs
+# - noisy_pattern
+# - volatile_pattern
+
+
+import numpy as np
+import random
+from frontendhelpers import *
+
+
+def define_reward(opt_p, n_trials=400, reward_mu=3, reward_std=1):
+
+    trial_index = np.arange(n_trials)
+
+    # define suboptimal choice reward probability
+    subopt_p = 1 - opt_p
+
+    # sample rewards
+    reward_values = np.random.normal(
+        loc=reward_mu, scale=reward_std, size=n_trials)
+
+    # calcualte n_trials based on probabilities
+    n_opt_reward_trials = int(opt_p * n_trials)
+    n_subopt_reward_trials = int(subopt_p * n_trials)
+
+    # find indices for optimal and suboptimal choices
+    opt_reward_idx = np.random.choice(
+        trial_index, size=n_opt_reward_trials, replace=False)
+    subopt_reward_idx = np.setxor1d(trial_index, opt_reward_idx)
+
+    # intialize reward vectors
+    reward_t1, reward_t2 = np.zeros((n_trials)), np.zeros((n_trials))
+
+    # assign rewards
+    reward_t1[opt_reward_idx] = reward_values[opt_reward_idx]
+    reward_t2[subopt_reward_idx] = reward_values[subopt_reward_idx]
+
+    return reward_t1, reward_t2
+
+
+def define_changepoints(n_trials, reward_t1, reward_t2, cp_lambda):
+
+    # find approximate number of change points
+    n_cps = int(n_trials / cp_lambda)
+    cp_base = np.cumsum(
+        np.random.poisson(
+            lam=cp_lambda,
+            size=n_cps))  # calculate cp indices
+
+    cp_idx = np.insert(cp_base, 0, 0)  # add 0
+    cp_idx = np.append(cp_idx, n_trials - 1)  # add 0
+
+    cp_idx = cp_idx[cp_idx < n_trials]
+
+    cp_indicator = np.zeros(n_trials)
+    cp_indicator[cp_idx] = 1
+
+    return cp_idx, cp_indicator
+
+
+def define_epochs(n_trials, reward_t1, reward_t2, cp_idx, opt_p):
+
+    t1_epochs = []
+    t2_epochs = []
+
+    subopt_p = 1 - opt_p
+
+    epoch_number = []
+    epoch_trial = []
+    epoch_length = []
+
+    reward_p = []
+
+    p_id_solution = []  # female greeble is always first
+
+    f_greeble = ord('f')
+    m_greeble = ord('m')
+
+    current_target = True
+    for i in range(len(cp_idx) - 1):
+        if current_target:
+            t1_epochs.append(reward_t1[cp_idx[i]:cp_idx[i + 1]])
+            t2_epochs.append(reward_t2[cp_idx[i]:cp_idx[i + 1]])
+            reward_p.append(np.repeat(opt_p, cp_idx[i + 1] - cp_idx[i]))
+            p_id_solution.append(
+                np.repeat(f_greeble, cp_idx[i + 1] - cp_idx[i]))
+        else:
+            t1_epochs.append(reward_t2[cp_idx[i]:cp_idx[i + 1]])
+            t2_epochs.append(reward_t1[cp_idx[i]:cp_idx[i + 1]])
+            reward_p.append(np.repeat(subopt_p, cp_idx[i + 1] - cp_idx[i]))
+            p_id_solution.append(
+                np.repeat(m_greeble, cp_idx[i + 1] - cp_idx[i]))
+
+        epoch_number.append(np.repeat(i, cp_idx[i + 1] - cp_idx[i]))
+        epoch_trial.append(np.arange(cp_idx[i + 1] - cp_idx[i]))
+        epoch_length.append(np.repeat(len(np.arange(
+            cp_idx[i + 1] - cp_idx[i])), repeats=len(np.arange(cp_idx[i + 1] - cp_idx[i]))))
+
+        if i == len(cp_idx) - 2:
+            if current_target:
+                t1_epochs.append(reward_t1[-1])
+                t2_epochs.append(reward_t2[-1])
+                reward_p.append(opt_p)
+                p_id_solution.append(f_greeble)
+            else:
+                t1_epochs.append(reward_t2[-1])
+                t2_epochs.append(reward_t1[-1])
+                reward_p.append(opt_p)
+                p_id_solution.append(m_greeble)
+
+            epoch_number.append(i)
+
+        current_target = not(current_target)
+
+    epoch_length[-1] = epoch_length[-1] + 1
+    # flatten
+    epoch_number = np.hstack(epoch_number).astype('float')
+    epoch_trial = np.hstack(epoch_trial).astype('float')
+    epoch_length = np.hstack(epoch_length).astype('float')
+
+    epoch_trial = np.append(epoch_trial, (epoch_trial[-1] + 1))
+    epoch_length = np.append(epoch_length, epoch_length[-1])
+
+    t1_epochs = np.hstack(t1_epochs)
+    t2_epochs = np.hstack(t2_epochs)
+    reward_p = np.hstack(reward_p).astype('float')
+    reward_p[-1] = reward_p[-2]
+    p_id_solution = np.hstack(p_id_solution)
+
+    # Matthew: new code
+    t1_epochs = np.divide(t1_epochs, 3)
+    t2_epochs = np.divide(t2_epochs, 3)
+    for i in range(0, len(t1_epochs)):
+        if random.uniform(0, 1) > opt_p:
+            temp = t1_epochs[i]
+            t1_epochs[i] = t2_epochs[i]
+            t2_epochs[i] = temp
+
+    return t1_epochs, t2_epochs, epoch_number, reward_p, p_id_solution, epoch_trial, epoch_length
+
+
+n_trials = 600
+volatility = 30
+conflict = 0.75
+
+
+(rt1, rt2) = define_reward(1, n_trials)
+(cp_idx, cp_indicator) = define_changepoints(n_trials, rt1, rt2, volatility)
+(t1_epochs, t2_epochs, epoch_number, reward_p, p_id_solution, epoch_trial,
+ epoch_length) = define_epochs(10, rt1, rt2, cp_idx, conflict)
+noisy_pattern = [min([.00001, abs(x)]) * 100000 for x in t1_epochs]
+volatile_pattern = [x % 2 for x in epoch_number]
