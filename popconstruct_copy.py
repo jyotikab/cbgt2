3fca708ba8f488e7c1f90f6fa3e810721ae2ed80
Matthew Clapp <clappm@users.noreply.github.com>
2021-08-23 09:47:08 -0400

    make helper_connectivity function

--- popconstruct_copy.py
+++ popconstruct_copy.py
@@ -58,55 +58,55 @@ def helper_poppathways(popdata, newpathways=None):
 
     simplepathways = pd.DataFrame(
         [
-            ['LIP', 'D1STR', 'AMPA', 'syn', 1, 0.027],
-            ['LIP', 'D1STR', 'NMDA', 'syn', 1, 0.027],
-            ['LIP', 'D2STR', 'AMPA', 'syn', 1, 0.027],
-            ['LIP', 'D2STR', 'NMDA', 'syn', 1, 0.027],
-            ['LIP', 'FSI', 'AMPA', 'all', 1, 0.198],
-            ['LIP', 'Th', 'AMPA', 'all', 1, 0.035],
-            ['LIP', 'Th', 'NMDA', 'all', 1, 0.035],
+            ['LIP', 'D1STR', 'AMPA', 'syn', 1, 0.027, True],
+            ['LIP', 'D1STR', 'NMDA', 'syn', 1, 0.027, False],
+            ['LIP', 'D2STR', 'AMPA', 'syn', 1, 0.027, True],
+            ['LIP', 'D2STR', 'NMDA', 'syn', 1, 0.027, False],
+            ['LIP', 'FSI', 'AMPA', 'all', 1, 0.198, False],
+            ['LIP', 'Th', 'AMPA', 'all', 1, 0.035, False],
+            ['LIP', 'Th', 'NMDA', 'all', 1, 0.035, False],
 
-            ['D1STR', 'D1STR', 'GABA', 'syn', 0.45, 0.28],
-            ['D1STR', 'D2STR', 'GABA', 'syn', 0.45, 0.28],
-            ['D1STR', 'GPi', 'GABA', 'syn', 1, 2.09],
+            ['D1STR', 'D1STR', 'GABA', 'syn', 0.45, 0.28, False],
+            ['D1STR', 'D2STR', 'GABA', 'syn', 0.45, 0.28, False],
+            ['D1STR', 'GPi', 'GABA', 'syn', 1, 2.09, False],
 
-            ['D2STR', 'D2STR', 'GABA', 'syn', 0.45, 0.28],
-            ['D2STR', 'D1STR', 'GABA', 'syn', 0.5, 0.28],
-            ['D2STR', 'GPeP', 'GABA', 'syn', 1, 4.07],
+            ['D2STR', 'D2STR', 'GABA', 'syn', 0.45, 0.28, False],
+            ['D2STR', 'D1STR', 'GABA', 'syn', 0.5, 0.28, False],
+            ['D2STR', 'GPeP', 'GABA', 'syn', 1, 4.07, False],
 
-            ['FSI', 'FSI', 'GABA', 'all', 1, 3.25833],
-            ['FSI', 'D1STR', 'GABA', 'all', 1, 1.7776],
-            ['FSI', 'D2STR', 'GABA', 'all', 1, 1.669867],
+            ['FSI', 'FSI', 'GABA', 'all', 1, 3.25833, False],
+            ['FSI', 'D1STR', 'GABA', 'all', 1, 1.7776, False],
+            ['FSI', 'D2STR', 'GABA', 'all', 1, 1.669867, False],
 
-            ['GPeP', 'GPeP', 'GABA', 'all', 0.0667, 1.75],
-            ['GPeP', 'STNE', 'GABA', 'syn', 0.0667, 0.35],
-            ['GPeP', 'GPi', 'GABA', 'syn', 1, 0.06],
+            ['GPeP', 'GPeP', 'GABA', 'all', 0.0667, 1.75, False],
+            ['GPeP', 'STNE', 'GABA', 'syn', 0.0667, 0.35, False],
+            ['GPeP', 'GPi', 'GABA', 'syn', 1, 0.06, False],
 
-            ['STNE', 'GPeP', 'AMPA', 'syn', 0.161668, 0.07],
-            ['STNE', 'GPeP', 'NMDA', 'syn', 0.161668, 1.51],
-            ['STNE', 'GPi', 'NMDA', 'all', 1, 0.038],
+            ['STNE', 'GPeP', 'AMPA', 'syn', 0.161668, 0.07, False],
+            ['STNE', 'GPeP', 'NMDA', 'syn', 0.161668, 1.51, False],
+            ['STNE', 'GPi', 'NMDA', 'all', 1, 0.038, False],
 
-            ['GPi', 'Th', 'GABA', 'syn', 1, 0.3315],
+            ['GPi', 'Th', 'GABA', 'syn', 1, 0.3315, False],
 
-            ['Th', 'D1STR', 'AMPA', 'syn', 1, 0.3825],
-            ['Th', 'D2STR', 'AMPA', 'syn', 1, 0.3825],
-            ['Th', 'FSI', 'AMPA', 'all', 0.8334, 0.1],
-            ['Th', 'LIP', 'NMDA', 'all', 0.8334, 0.03],
+            ['Th', 'D1STR', 'AMPA', 'syn', 1, 0.3825, False],
+            ['Th', 'D2STR', 'AMPA', 'syn', 1, 0.3825, False],
+            ['Th', 'FSI', 'AMPA', 'all', 0.8334, 0.1, False],
+            ['Th', 'LIP', 'NMDA', 'all', 0.8334, 0.03, False],
 
             # ramping ctx
 
-            ['LIP', 'LIP', 'AMPA', 'all', 0.4335, 0.0127],
-            ['LIP', 'LIP', 'NMDA', 'all', 0.4335, 0.15],
-            ['LIP', 'LIPI', 'AMPA', 'all', 0.241667, 0.113],
-            ['LIP', 'LIPI', 'NMDA', 'all', 0.241667, 0.525],
+            ['LIP', 'LIP', 'AMPA', 'all', 0.4335, 0.0127, False],
+            ['LIP', 'LIP', 'NMDA', 'all', 0.4335, 0.15, False],
+            ['LIP', 'LIPI', 'AMPA', 'all', 0.241667, 0.113, False],
+            ['LIP', 'LIPI', 'NMDA', 'all', 0.241667, 0.525, False],
 
-            ['LIPI', 'LIP', 'GABA', 'all', 1, 1.75],
-            ['LIPI', 'LIPI', 'GABA', 'all', 1, 3.58335],
+            ['LIPI', 'LIP', 'GABA', 'all', 1, 1.75, False],
+            ['LIPI', 'LIPI', 'GABA', 'all', 1, 3.58335, False],
 
-            ['Th', 'LIPI', 'NMDA', 'all', 0.8334, 0.015],
+            ['Th', 'LIPI', 'NMDA', 'all', 0.8334, 0.015, False],
 
         ],
-        columns=['src', 'dest', 'receptor', 'type', 'con', 'eff']
+        columns=['src', 'dest', 'receptor', 'type', 'con', 'eff', 'plastic']
     )
     simplepathways = trace(simplepathways, 'init')
 
@@ -127,68 +127,93 @@ def helper_poppathways(popdata, newpathways=None):
 
     return pathways
 
-
-def helper_connectivityAMPA(popdata, pathways):
-
-    connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
-    connectiongrid = trace(connectiongrid, 'init')
-
-    connectivity_AMPA = connectiongrid.copy()
-    meanEff_AMPA = connectiongrid.copy()
-
-    for idx, row in pathways.iterrows():
-        biselector = row['biselector']
-        receptor = row['receptor']
-        con = row['con']
-        eff = row['eff']
-
-        connectivity_AMPA = FillGridSelection(
-            connectivity_AMPA, popdata, biselector, con)
-        meanEff_AMPA = FillGridSelection(
-            meanEff_AMPA, popdata, biselector, eff)
-
-    return connectivity_AMPA, meanEff_AMPA
-
-
-def helper_connectivityGABA(popdata, pathways):
+def helper_connectivity(receptor, popdata, pathways):
 
     connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
     connectiongrid = trace(connectiongrid, 'init')
 
-    connectivity_GABA = connectiongrid.copy()
-    meanEff_GABA = connectiongrid.copy()
+    connectivity = connectiongrid.copy()
+    meanEff = connectiongrid.copy()
+    plasticity = connectiongrid.copy()
 
     for idx, row in pathways.iterrows():
-        biselector = row['biselector']
-        receptor = row['receptor']
-        con = row['con']
-        eff = row['eff']
+        if row['receptor'] == receptor:
+            biselector = row['biselector']
+            receptor = row['receptor']
+            con = row['con']
+            eff = row['eff']
+            plastic = row['plastic']
 
-        connectivity_GABA = FillGridSelection(
-            connectivity_GABA, popdata, biselector, con)
-        meanEff_GABA = FillGridSelection(
-            meanEff_GABA, popdata, biselector, eff)
+            connectivity = FillGridSelection(
+                connectivity, popdata, biselector, con)
+            meanEff = FillGridSelection(
+                meanEff, popdata, biselector, eff)
+            plasticity = FillGridSelection(
+                plasticity, popdata, biselector, plastic)
 
-    return connectivity_GABA, meanEff_GABA
+    return connectivity, meanEff, plasticity
 
-
-def helper_connectivityNMDA(popdata, pathways):
-
-    connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
-    connectiongrid = trace(connectiongrid, 'init')
-
-    connectivity_NMDA = connectiongrid.copy()
-    meanEff_NMDA = connectiongrid.copy()
-
-    for idx, row in pathways.iterrows():
-        biselector = row['biselector']
-        receptor = row['receptor']
-        con = row['con']
-        eff = row['eff']
-
-        connectivity_NMDA = FillGridSelection(
-            connectivity_NMDA, popdata, biselector, con)
-        meanEff_NMDA = FillGridSelection(
-            meanEff_NMDA, popdata, biselector, eff)
-
-    return connectivity_NMDA, meanEff_NMDA
+# def helper_connectivityAMPA(popdata, pathways):
+#
+#     connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
+#     connectiongrid = trace(connectiongrid, 'init')
+#
+#     connectivity_AMPA = connectiongrid.copy()
+#     meanEff_AMPA = connectiongrid.copy()
+#
+#     for idx, row in pathways.iterrows():
+#         biselector = row['biselector']
+#         receptor = row['receptor']
+#         con = row['con']
+#         eff = row['eff']
+#
+#         connectivity_AMPA = FillGridSelection(
+#             connectivity_AMPA, popdata, biselector, con)
+#         meanEff_AMPA = FillGridSelection(
+#             meanEff_AMPA, popdata, biselector, eff)
+#
+#     return connectivity_AMPA, meanEff_AMPA
+#
+#
+# def helper_connectivityGABA(popdata, pathways):
+#
+#     connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
+#     connectiongrid = trace(connectiongrid, 'init')
+#
+#     connectivity_GABA = connectiongrid.copy()
+#     meanEff_GABA = connectiongrid.copy()
+#
+#     for idx, row in pathways.iterrows():
+#         biselector = row['biselector']
+#         receptor = row['receptor']
+#         con = row['con']
+#         eff = row['eff']
+#
+#         connectivity_GABA = FillGridSelection(
+#             connectivity_GABA, popdata, biselector, con)
+#         meanEff_GABA = FillGridSelection(
+#             meanEff_GABA, popdata, biselector, eff)
+#
+#     return connectivity_GABA, meanEff_GABA
+#
+#
+# def helper_connectivityNMDA(popdata, pathways):
+#
+#     connectiongrid = constructSquareDf(untrace(popdata['name'].tolist()))
+#     connectiongrid = trace(connectiongrid, 'init')
+#
+#     connectivity_NMDA = connectiongrid.copy()
+#     meanEff_NMDA = connectiongrid.copy()
+#
+#     for idx, row in pathways.iterrows():
+#         biselector = row['biselector']
+#         receptor = row['receptor']
+#         con = row['con']
+#         eff = row['eff']
+#
+#         connectivity_NMDA = FillGridSelection(
+#             connectivity_NMDA, popdata, biselector, con)
+#         meanEff_NMDA = FillGridSelection(
+#             meanEff_NMDA, popdata, biselector, eff)
+#
+#     return connectivity_NMDA, meanEff_NMDA
